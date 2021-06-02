b04020fc558a4998f99a9e5ff174e812d649c9bc
vuongvgc <vuongvgc@gmail.com>
2021-06-02 22:34:40 +0700

    create simple route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 /////////////////////////
 //////     FILE    /////
 ///////////////////////
@@ -33,8 +34,19 @@ const http = require("http");
 //////    SERVER    /////
 ///////////////////////
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("The message from the server ! Yumy");
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is overView");
+  } else if (pathName === "/product") {
+    res.end("this is page product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello World",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("The server is running");
